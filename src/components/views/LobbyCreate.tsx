1a8d6fb5a101e47c3886917fdb62096c6eece799
Eric Rudischhauser <ericrudischhauser@MBP-von-Eric.fritz.box>
2024-04-25 23:14:21 +0200

    added settings form to lobby create page #30

--- src/components/views/LobbyCreate.tsx
+++ src/components/views/LobbyCreate.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Lobby from "models/Lobby";
 import { useNavigate } from "react-router-dom";
 import { Button } from "components/ui/Button";
@@ -7,15 +7,52 @@ import "styles/views/Lobby.scss";
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 
+const FormField = React.memo((props) => {
+  return (
+    <div className="join field">
+      <label className="join label">{props.label}</label>
+      <input
+        type={props.type || "text"}
+        className="register input"
+        placeholder={props.placeholder || "enter here.."}
+        name={props.name}
+        key={props.key}
+        value={props.value}
+        onChange={props.onChange}
+      />
+    </div>
+  );
+});
+
+FormField.displayName = "FormField";
+
+FormField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  key: PropTypes.string,
+  placeholder: PropTypes.string,
+};
+
 const LobbyCreate = () => {
   const navigate = useNavigate();
-  // const [lobbyId, setLobbyId] = useState<string>(null);
+  const [formData, setFormData] = useState({
+    maxAmtUsers: 50,
+    amtOfRounds: 1,
+    timeLimit: 60,
+  });
 
   const createLobby = async () => {
     try {
       const userToken = localStorage.getItem("userToken");
-      const requestBody = JSON.stringify({ userToken });
-      const response = await api.post("/lobby/create", requestBody);
+      const config = {
+        headers: {
+          userToken,
+        },
+      };
+      const response = await api.post("/lobby/create", formData, config);
 
       const lobby = new Lobby(response.data);
 
@@ -27,11 +64,52 @@ const LobbyCreate = () => {
     }
   };
 
+  const handleChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  }, []);
+
   return (
-    <BaseContainer className="home container">
-      <Button className="createLobbyButton" onClick={createLobby}>
-        Create Lobby
-      </Button>
+    <BaseContainer>
+      <div className="join container">
+        <div className="join form">
+          <FormField
+            label="Amount of rounds"
+            type="number"
+            name="amtOfRounds"
+            key="amtOfRounds"
+            placeholder="enter a number.."
+            value={formData.amtOfRounds}
+            onChange={handleChange}
+          />
+          <FormField
+            label="Time limit to guess in seconds"
+            type="number"
+            name="timeLimit"
+            key="timeLimit"
+            placeholder="enter a number.."
+            value={formData.timeLimit}
+            onChange={handleChange}
+          />
+          <FormField
+            label="Maxiumum amount of users"
+            type="number"
+            name="maxAmtUsers"
+            key="maxAmtUsers"
+            placeholder="enter a number.."
+            value={formData.maxAmtUsers}
+            onChange={handleChange}
+          />
+          <div className="join button-container">
+            <Button width="50%" onClick={() => createLobby()}>
+              Create Lobby
+            </Button>
+          </div>
+        </div>
+      </div>
     </BaseContainer>
   );
 };

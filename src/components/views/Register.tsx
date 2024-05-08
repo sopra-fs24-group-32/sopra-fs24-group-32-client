ed1a566b43482ba5becb47d7e565c471aafc0e9f
rogerjeasy <rogerjeasy@gmail.com>
2024-05-08 17:29:54 +0200

    updating proper error messages

--- src/components/views/Register.tsx
+++ src/components/views/Register.tsx
@@ -57,8 +57,16 @@ const Register = () => {
 
       navigate("/home");
     } catch (error) {
+      console.error(
+        `Something went wrong while joining the lobby: \n${handleError(error)}`
+      );
+      console.error("Details:", error);
+      const errorMessage =
+        error.response?.data?.message ||
+        error.message ||
+        "An unknown error occurred";
       alert(
-        `Something went wrong during the register: \n${handleError(error)}`
+        `${errorMessage}`
       );
     }
   };

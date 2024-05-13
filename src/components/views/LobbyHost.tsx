6e45e9bc69c4577d34f79a9a665293a0db804de3
Eric Rudischhauser <ericrudischhauser@MBP-von-Eric.fritz.box>
2024-05-14 00:01:00 +0200

    change user appearance #72

--- src/components/views/LobbyHost.tsx
+++ src/components/views/LobbyHost.tsx
@@ -374,43 +374,67 @@ const LobbyDetailHost = () => {
               </div>
             </div>
           </li>
-          {lobby.users &&
-            lobby.users.map((player, index) => (
-              <li
-                key={`player-${index}`}
-                style={{
-                  backgroundColor: "#f0f0f0",
-                  marginBottom: "10px",
-                  borderRadius: "5px",
-                  padding: "10px",
-                }}
-              >
+          {lobby.users && (
+            <li
+              style={{
+                backgroundColor: "#f0f0f0",
+                marginBottom: "10px",
+                borderRadius: "5px",
+                padding: "10px",
+                display: "flex",
+                gap: "10px",
+                maxWidth: "320px",
+                flexWrap: "wrap",
+              }}
+            >
+              {lobby.users.map((player, index) => (
                 <div
+                  key={`player-${index}`}
                   className="player container tooltip"
                   style={{
                     display: "flex",
-                    flexDirection: "column",
                     justifyContent: "center",
                     alignItems: "center",
+                    width: "fit-content",
+                    margin: "0",
+                    zIndex: "1",
                   }}
                 >
                   <div
                     className="player-username"
-                    style={{ fontWeight: "bold", marginBottom: "5px" }}
+                    style={{ fontWeight: "bold" }}
                   >
                     {player.username}
                   </div>
                   {player.userToken !== localStorage.getItem("userToken") && (
-                    <Button
+                    <button
                       width="100%"
                       style={{
-                        marginBottom: "10px",
-                        backgroundColor: "#ff6666",
+                        backgroundColor: "transparent",
+                        border: "none",
                       }}
                       onClick={() => kickPlayer(player.userToken)}
                     >
-                      Kick Out
-                    </Button>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                        style={{
+                          height: "24px",
+                          width: "24px",
+                          stroke: "red",
+                        }}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                        />
+                      </svg>
+                    </button>
                   )}
                   {/* <div className="player-score">Score: {player.score}</div> */}
                   <span className="tooltip-text">
@@ -442,8 +466,9 @@ const LobbyDetailHost = () => {
                     )}
                   </span>
                 </div>
-              </li>
-            ))}
+              ))}
+            </li>
+          )}
         </ul>
 
         <Button

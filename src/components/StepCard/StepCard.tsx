c0ba827ac1a35cceeb43899f70e6afedfbe9015e
rogerjeasy <rogerjeasy@gmail.com>
2025-02-26 19:08:37 +0100

    implementation of the step card component

--- src/components/StepCard/StepCard.tsx
+++ src/components/StepCard/StepCard.tsx
@@ -4,17 +4,26 @@ import PropTypes from 'prop-types';
 import './StepCard.scss';
 
 const StepCard = ({ number, title, description, icon }) => {
-  return (
-    <div className="step-card">
-      <div className="step-card__number">{number}</div>
-      <div className="step-card__icon">
-        {icon}
+    return (
+      <div className="step-card">
+        <div className="step-card__number">{number}</div>
+        
+        <div className="step-card__icon">
+          {icon}
+        </div>
+        
+        <h3 className="step-card__title">{title}</h3>
+        
+        <p className="step-card__description">{description}</p>
+        
+        <div className="step-card__dots">
+          <span className="step-card__dot"></span>
+          <span className="step-card__dot"></span>
+          <span className="step-card__dot"></span>
+        </div>
       </div>
-      <h3 className="step-card__title">{title}</h3>
-      <p className="step-card__description">{description}</p>
-    </div>
-  );
-};
+    );
+  };
 
 StepCard.propTypes = {
   number: PropTypes.number.isRequired,

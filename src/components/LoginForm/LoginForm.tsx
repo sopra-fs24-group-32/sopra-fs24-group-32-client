208a259e46343eddb21e1a690217fab2e23f4cba
rogerjeasy <rogerjeasy@gmail.com>
2025-02-27 22:46:23 +0100

    updated the animation in the close button

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from 'react';
-import PropTypes from 'prop-types';
 import './LoginForm.scss';
 import { useNavigate } from "react-router-dom";
 import { SignedIn, SignedOut, SignIn, SignUp, useClerk, UserButton, useUser } from "@clerk/clerk-react";

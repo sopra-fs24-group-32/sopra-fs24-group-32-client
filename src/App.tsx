a119e29bd9879c72101de1ccda62fe358893015b
rogerjeasy <rogerjeasy@gmail.com>
2025-03-01 23:56:55 +0100

    added redux state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,53 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import Header from "./components/layout/Header";
 import AppRouter from "./components/routing/routers/AppRouter";
 import { WebSocketProvider } from "./helpers/WebSocketContext";
 import { AuthProvider } from "./components/context/AuthContext";
-import Footer from "components/layout/Footer";
+import { AuthReduxBridge } from "./components/context/AuthReduxBridge";
+import { WebSocketReduxBridge } from "./helpers/WebSocketReduxBridge";
+import Footer from "./components/layout/Footer";
 import "./App.scss";
-import { BrowserRouter } from "react-router-dom";
 
+/**
+ * Main App component that sets up the application with all necessary providers
+ * Provider hierarchy:
+ * 1. Redux Provider (global state management)
+ * 2. WebSocketProvider (real-time communication)
+ * 3. BrowserRouter (routing)
+ * 4. AuthProvider (authentication)
+ * 5. Integration bridges (connecting contexts with Redux)
+ */
 const App = () => {
-  return (
+  // Define the app content
+  const appContent = (
     <WebSocketProvider>
       <BrowserRouter>
         <AuthProvider>
-          <div className="app-container">
-            <Header />
-            <main className="main-content">
-              <AppRouter />
-            </main>
-            <Footer />
-          </div>
+          {/* Bridge components to integrate contexts with Redux */}
+          <AuthReduxBridge>
+            <WebSocketReduxBridge>
+              <div className="app-container">
+                <Header />
+                <main className="main-content">
+                  <AppRouter />
+                </main>
+                <Footer />
+              </div>
+            </WebSocketReduxBridge>
+          </AuthReduxBridge>
         </AuthProvider>
       </BrowserRouter>
     </WebSocketProvider>
+  );
+
+  // Render with children as the third argument to React.createElement
+  return React.createElement(
+    Provider,
+    { store: store },
+    appContent
   );
 };
 
